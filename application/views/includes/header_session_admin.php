965e03378e38a7cf8d8fe947e0d8310f33293df3
Anrogo <antoniorogo96@gmail.com>
2020-05-06 18:15:25 +0200

    probando commit

--- application/views/includes/header_session_admin.php
+++ application/views/includes/header_session_admin.php
@@ -11,7 +11,7 @@
                             </svg>Panel de control</a>
                     </div>
                     <div class="col-6 col-sm-3 text-center hijo">
-                        <a href="/perfil-usuario" class="btn btn-lg btn-outline-primary botones"><svg class="bi bi-person-square" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <a href="/perfil-admin" class="btn btn-lg btn-outline-primary botones"><svg class="bi bi-person-square" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" d="M14 1H2a1 1 0 00-1 1v12a1 1 0 001 1h12a1 1 0 001-1V2a1 1 0 00-1-1zM2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2z" clip-rule="evenodd" />
                                 <path fill-rule="evenodd" d="M2 15v-1c0-1 1-4 6-4s6 3 6 4v1H2zm6-6a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
                             </svg>Mi perfil</a>
